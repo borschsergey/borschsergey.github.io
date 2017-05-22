601ae9c6781c4c5b434e17af78e38c8ab4c3065d
BorschSergey <kailketch@gmail.com>
2017-05-22 20:00:45 +0300

    Revert "Update tooltip.js"
    This reverts commit 2e0a5328e0d31d0541b2bb1cc5de986fca96060c.

--- ALPFA-version-2.0-portfolio-0/source/scripts/tooltip.js
+++ ALPFA-version-2.0-portfolio-0/source/scripts/tooltip.js
@@ -1,33 +1,23 @@
-function windWidthSize() {
-  if ($(window).width() <= '720') {
-    $(".rectangle__contact__numb__coll__elem")
-    .hover(function () {
-        $(this).clone().css({
-                  'font-size':'10px',
-                  'background': 'rgba(17,17,17,.9)',
-                  'position': 'absolute',
-                  'z-index': '10',
-                  'top': '-20px',
-                  'left': '20%',
-                  'border-radius': '20px',
-                  'color': '#f1f1f1',
-                  'padding': '5px',
-                  'padding-left': '30px'
-                })
-          .appendTo('.rectangle__contact__numb');
-        },
-    function () {
-      $(this)
-          .parent()
-          .parent()
-          .find('.rectangle__contact__numb__coll__elem[style]')
-          .detach()
-    });
-  } else {
-    return;
-  }
-};
-
-$(window).on('load resize', function(event) {
-  windWidthSize();
+$(".rectangle__contact__numb__coll__elem")
+.hover(function () {
+    $(this).clone().css({
+							'font-size':'10px',
+							'background': 'rgba(17,17,17,.9)',
+							'position': 'absolute',
+							'z-index': '10',
+							'top': '-20px',
+							'left': '20%',
+							'border-radius': '20px',
+							'color': '#f1f1f1',
+							'padding': '5px',
+							'padding-left': '30px'
+						})
+      .appendTo('.rectangle__contact__numb');
+    },
+function () {
+	$(this)
+			.parent()
+			.parent()
+			.find('.rectangle__contact__numb__coll__elem[style]')
+			.detach()
 });
